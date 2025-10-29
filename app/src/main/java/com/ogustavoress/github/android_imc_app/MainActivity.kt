9648691338e49d2169a28603598bc376acedac49
Gustavo <ogustavoress@gmail.com>
2025-10-29 19:57:59 -0300

    feat: incluindo componente Image

--- app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
+++ app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.ogustavoress.github.android_imc_app.ui.theme.AndroidimcappTheme
 
@@ -44,7 +48,13 @@ fun ImcScreen(modifier: Modifier = Modifier) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = modifier.fillMaxWidth().height(160.dp)
             ) {
-                /*TODO*/
+                Image(
+                    painter = painterResource(id = R.drawable.bmi),
+                    contentDescription = "logo",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(top = 16.dp)
+                )
             }
             //FORMULÁRIO
             Column(
