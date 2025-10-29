f6154738638b4096973be5670b5771b1f5077cba
Gustavo <ogustavoress@gmail.com>
2025-10-29 18:59:06 -0300

    feat: criando a imc-screen

--- app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
+++ app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ogustavoress.github.android_imc_app.ui.theme.AndroidimcappTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +24,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidimcappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    ImcScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ImcScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            //HEADER
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier.fillMaxWidth().height(160.dp)
+            ) {
+                /*TODO*/
+            }
+            //FORMULÁRIO
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp)
+            ) {
+                /*TODO*/
+            }
+        }
+
     }
 }
