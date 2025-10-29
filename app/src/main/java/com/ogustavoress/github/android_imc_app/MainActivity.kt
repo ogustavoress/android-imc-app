f06701b9761bc46bdc3731fc13acfa79f5cf73af
Gustavo <ogustavoress@gmail.com>
2025-10-29 20:16:12 -0300

    feat: trabalhando com Cards

--- app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
+++ app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
@@ -8,11 +8,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +31,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ogustavoress.github.android_imc_app.ui.theme.AndroidimcappTheme
@@ -75,7 +86,89 @@ fun ImcScreen(modifier: Modifier = Modifier) {
             Column(
                 modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp)
             ) {
-                /*TODO*/
+                Card(
+                    modifier = Modifier
+                        .offset(y = (-30).dp)
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xfff9f6f6)),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+                    Text(
+                        text = "Seus dados",
+                        modifier = Modifier.fillMaxWidth(),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.vermelho_fiap),
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(
+                        text = "Seu peso",
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = colorResource(id = R.color.vermelho_fiap)
+                    )
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = {
+                            Text(text = "Seu peso em Kg.")
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            unfocusedBorderColor = colorResource(id = R.color.vermelho_fiap),
+                            focusedBorderColor = colorResource(id = R.color.vermelho_fiap)
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Sua altura",
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = colorResource(id = R.color.vermelho_fiap)
+                    )
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = {
+                            Text(text = "Sua altura em cm.")
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            unfocusedBorderColor = colorResource(id = R.color.vermelho_fiap),
+                            focusedBorderColor = colorResource(id = R.color.vermelho_fiap)
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType =
+                                KeyboardType.Decimal
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp),
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =
+                                colorResource(id = R.color.vermelho_fiap)
+                    ) {
+                        Text(
+                            text = "CALCULAR",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            fontSize = 14.sp
+                        )
+                    }
+
+                }
             }
         }
 
