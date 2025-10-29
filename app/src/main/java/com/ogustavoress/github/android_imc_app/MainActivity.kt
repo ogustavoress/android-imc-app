26d9193e0ab2e48706249e0257a103106da18038
Gustavo <ogustavoress@gmail.com>
2025-10-29 20:06:12 -0300

    feat: configurando o arquivo colors.xml

--- app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
+++ app/src/main/java/com/ogustavoress/github/android_imc_app/MainActivity.kt
@@ -14,11 +14,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ogustavoress.github.android_imc_app.ui.theme.AndroidimcappTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +51,10 @@ fun ImcScreen(modifier: Modifier = Modifier) {
             //HEADER
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = modifier.fillMaxWidth().height(160.dp)
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(160.dp)
+                    .background(colorResource(id = R.color.vermelho_fiap))
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.bmi),
@@ -54,6 +62,13 @@ fun ImcScreen(modifier: Modifier = Modifier) {
                     modifier = Modifier
                         .size(100.dp)
                         .padding(top = 16.dp)
+                )
+                Text(
+                    text = "Calculadora IMC",
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 12.dp, bottom = 24.dp)
                 )
             }
             //FORMULÁRIO
